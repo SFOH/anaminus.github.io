c727438e827aabd29a5a1a9e249a442d43a88f52
Sam Hanson <humbagel@gmail.com>
2013-10-19 19:52:03 -0500

    formatted

--- api/search.js
+++ api/search.js
@@ -1,50 +1,44 @@
-$(document).ready(function(){
-	console.log("READY");
-
-
+$(document).ready(function() {
 	var searchInput = $("#search-input");
 	var boxContent = $("#box-content");
 
 	var fuse;
-	$.getJSON('/api/search-db.json',function(data){
-		fuse = new Fuse(data,{
-			keys : ['search'],
-			threshold : 0.3
+	$.getJSON('/api/search-db.json', function(data) {
+		fuse = new Fuse(data, {
+			keys: ['search'],
+			threshold: 0.3
 		})
 	})
 
+	var searchResults = $('<div id="search-results"></div>').appendTo('#ref-box');
+	var resultContainer = $('<ul></ul>').appendTo(searchResults);
 
-	var searchResults = $('<div id="search-results"></div>').appendTo('#ref-box')
-	var resultContainer = $('<ul></ul>').appendTo(searchResults)
-
-	function doSearch(){
-		console.log("SEARCH");
+	function doSearch() {
 		if (fuse == undefined) {
-			console.log("NOPE");
-			return
+			return;
 		}
 		var text = searchInput.val();
 
-		resultContainer.empty()
+		resultContainer.empty();
 
 		if (text.length == 0) {
-			searchResults.css('display','none')
-			boxContent.css('display','block')
+			searchResults.css('display', 'none')
+			boxContent.css('display', 'block')
 		} else {
-			searchResults.css('display','block')
-			boxContent.css('display','none')
+			searchResults.css('display', 'block')
+			boxContent.css('display', 'none')
 
-			var results = fuse.search(text)
-			var max = results.length > 50 ? 50 : results.length
-			for (i = 0; i < max; i++){
-				resultContainer.append('<li><a href="'+results[i].url+'">'+results[i].name+'</a></li>')
+			var results = fuse.search(text);
+			var max = results.length > 50 ? 50 : results.length;
+			for (i = 0; i < max; i++) {
+				resultContainer.append('<li><a href="' + results[i].url + '">' + results[i].name + '</a></li>');
 			}
 		}
 	}
 
 	var timer;
-	searchInput.on("input",function(){
+	searchInput.on("input", function() {
 		timer && clearTimeout(timer);
-		timer = window.setTimeout(doSearch,200);
+		timer = window.setTimeout(doSearch, 200);
 	})
 })
